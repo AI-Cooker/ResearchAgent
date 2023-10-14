62e505ca7196bfb156ac1bbc04280afb90dff6b3
Nguyen Hoang Anh Tu <nguyenhoanganhtu2406@gmail.com>
2023-10-14 15:01:23 +0700

    set cookie after generating

--- web_server/main.py
+++ web_server/main.py
@@ -32,9 +32,10 @@ class RequestBody(BaseModel):
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/question")
-async def ask_question(request_body: RequestBody, session_id: str = Depends(get_session_id)):
+@app.post("/ask_question")
+async def ask_question(response: Response, request_body: RequestBody, session_id: str = Depends(get_session_id)):
     print(f"ask_question: {session_id}")
+    response.set_cookie(key="Authorization", value=session_id)
     long_input = request_body.user_input
     answer = pass_to_prompt(long_input, session_id)
     return {"answer": answer}
@@ -48,61 +49,3 @@ async def new_chat(request: Request, response: Response):
     SESSION_DB.append(session_id)
     response.set_cookie(key="Authorization", value=session_id)
     return {"session_id": session_id}
-
-
-# @app.get("/testsession")
-# def get_auth_user(request: Request):
-#     """verify that user has a valid session"""
-#     session_id = request.cookies.get("Authorization")
-#     if not session_id:
-#         raise HTTPException(status_code=401)
-#     if session_id not in SESSION_DB:
-#         raise HTTPException(status_code=403)
-#     return True
-
-
-# from fastapi import Request, Depends, HTTPException, Response
-# from fastapi.responses import RedirectResponse
-
-# # This must be randomly generated
-# RANDON_SESSION_ID = "iskksioskassyidd"
-
-# # This must be a lookup on user database
-# USER_CORRECT = ("admin", "admin")
-
-# # This must be Redis, Memcached, SQLite, KV, etc...
-# SESSION_DB = {}
-
-
-# @app.post("/login")
-# async def session_login(username: str, password: str):
-#     """/login?username=ssss&password=1234234234"""
-#     allow = (username, password) == USER_CORRECT
-#     if allow is False:
-#         raise HTTPException(status_code=401)
-#     response = RedirectResponse("/", status_code=302)
-#     response.set_cookie(key="Authorization", value=RANDON_SESSION_ID)
-#     SESSION_DB[RANDON_SESSION_ID] = username
-#     return response
-
-
-# @app.post("/logout")
-# async def session_logout(response: Response):
-#     response.delete_cookie(key="Authorization")
-#     SESSION_DB.pop(RANDON_SESSION_ID, None)
-#     return {"status": "logged out"}
-
-
-# def get_auth_user(request: Request):
-#     """verify that user has a valid session"""
-#     session_id = request.cookies.get("Authorization")
-#     if not session_id:
-#         raise HTTPException(status_code=401)
-#     if session_id not in SESSION_DB:
-#         raise HTTPException(status_code=403)
-#     return True
-
-
-# @app.get("/", dependencies=[Depends(get_auth_user)])
-# async def secret():
-#     return {"secret": "info"}
